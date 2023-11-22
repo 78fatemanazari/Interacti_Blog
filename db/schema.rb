beea0311ebe8d6e7b7ab45679e099a05856eb2ad
fatemanazari <fatemanazari78@gmail.com>
2023-11-23 00:05:04 +0330

    Use migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,47 +10,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_231_122_000_000) do
-  enable_extension 'plpgsql'
+ActiveRecord::Schema[7.1].define(version: 0) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
-  create_table 'users', force: :cascade do |t|
-    t.string 'name'
-    t.string 'photo_url'
-    t.text 'bio'
-    t.integer 'posts_counter', default: 0
-    t.timestamps
-  end
-
-  create_table 'posts', force: :cascade do |t|
-    t.integer 'author_id'
-    t.string 'title'
-    t.text 'text'
-    t.integer 'comments_counter', default: 0
-    t.integer 'likes_counter', default: 0
-    t.timestamps
-  end
-
-  create_table 'comments', force: :cascade do |t|
-    t.integer 'user_id'
-    t.integer 'post_id'
-    t.text 'text'
-    t.timestamps
-  end
-
-  create_table 'likes', force: :cascade do |t|
-    t.integer 'user_id'
-    t.integer 'post_id'
-    t.timestamps
-  end
-
-  add_foreign_key 'posts', 'users', column: 'author_id'
-  add_index 'posts', ['author_id'], name: 'index_posts_on_author_id'
-
-  add_foreign_key 'comments', 'users', column: 'user_id'
-  add_foreign_key 'comments', 'posts', column: 'post_id'
-  add_index 'comments', %w[user_id post_id], name: 'index_comments_on_user_id_and_post_id', unique: true
-
-  add_foreign_key 'likes', 'users', column: 'user_id'
-  add_foreign_key 'likes', 'posts', column: 'post_id'
-  add_index 'likes', %w[user_id post_id], name: 'index_likes_on_user_id_and_post_id', unique: true
 end
