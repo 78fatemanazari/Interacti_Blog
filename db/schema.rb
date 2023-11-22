f9b3d9f364d7cd0f8010daa049cede4aea6e10ec
fatemanazari <fatemanazari78@gmail.com>
2023-11-22 21:40:35 +0330

    Modify remote repo

--- db/schema.rb
+++ db/schema.rb
@@ -10,47 +10,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_11_22_000000) do
+ActiveRecord::Schema[7.1].define(version: 0) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-  create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.string "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.atlassian.com%2Fagile%2Fproject-management%2Fuser-stories&psig=AOvVaw1J-xVxhojIpVc0TBZFyJ8E&ust=1700761895664000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCPiD7v6V2IIDFQAAAAAdAAAAABAE"
-    t.text "bio"
-    t.integer "posts_counter", default: 0
-    t.timestamps
-  end
 
-  create_table "posts", force: :cascade do |t|
-    t.integer "author_id"
-    t.string "title"
-    t.text "text"
-    t.integer "comments_counter", default: 0
-    t.integer "likes_counter", default: 0
-    t.timestamps
-  end
-
-  create_table "comments", force: :cascade do |t|
-    t.integer "user_id"
-    t.integer "post_id"
-    t.text "text"
-    t.timestamps
-  end
-
-  create_table "likes", force: :cascade do |t|
-    t.integer "user_id"
-    t.integer "post_id"
-    t.timestamps
-  end
-
-  add_foreign_key "posts", "users", column: "author_id"
-  add_index "posts", ["author_id"], name: "index_posts_on_author_id"
-
-  add_foreign_key "comments", "users", column: "user_id"
-  add_foreign_key "comments", "posts", column: "post_id"
-  add_index "comments", ["user_id", "post_id"], name: "index_comments_on_user_id_and_post_id", unique: true
-
-  add_foreign_key "likes", "users", column: "user_id"
-  add_foreign_key "likes", "posts", column: "post_id"
-  add_index "likes", ["user_id", "post_id"], name: "index_likes_on_user_id_and_post_id", unique: true
 end
