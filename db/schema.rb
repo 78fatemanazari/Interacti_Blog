a8ede3e0112ec6d21795e04d189485edb44e6c48
fatemanazari <fatemanazari78@gmail.com>
2023-11-22 21:53:55 +0330

    Create database cridential

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 0) do
+ActiveRecord::Schema.define(version: 2023_11_22_000000) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
