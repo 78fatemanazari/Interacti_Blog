9a922aff6c97e998ba512db6a94c8c6545daa3ad
fatemanazari <fatemanazari78@gmail.com>
2023-11-24 09:07:38 +0330

    Make required changes

--- db/migrate/20231123075318_create_posts.rb
+++ db/migrate/20231123075318_create_posts.rb
@@ -3,7 +3,7 @@
 class CreatePosts < ActiveRecord::Migration[7.1]
   def change
     create_table :posts do |t|
-      t.references :author, null: false, foreign_key: true
+      t.references :author, foreign_key: { to_table: 'users' }, null: false
       t.string :title
       t.text :text
       t.integer :comments_counter
