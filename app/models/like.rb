968e79cf759531081be37fa899501ff1df0a3d57
fatemanazari <fatemanazari78@gmail.com>
2023-11-25 01:00:25 +0330

    Add test for the like model

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,12 +1,12 @@
-# app/models/like.rb
 class Like < ApplicationRecord
   belongs_to :user
   belongs_to :post
 
-  after_save :update_post_likes_counter
-  after_destroy :update_post_likes_counter
+  after_save :update_like_counter
 
-  def update_post_likes_counter
-    post.update_likes_counter
+  private
+
+  def update_like_counter
+    post.update(likes_counter: post.likes.count)
   end
 end
