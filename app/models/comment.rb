3c788050d821f3d6fc873020a0ed0fc41d9b86f0
fatemanazari <fatemanazari78@gmail.com>
2023-11-24 10:42:24 +0330

    Add active record callback

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -4,7 +4,15 @@ class Comment < ApplicationRecord
 
   validates :text, presence: true
 
-  def update_post_comments_counter
-    post.update(comments_counter: post.comments.count)
+  class Comment < ApplicationRecord
+    belongs_to :user
+    belongs_to :post
+
+    after_save :update_post_comments_counter
+    after_destroy :update_post_comments_counter
+
+    def update_post_comments_counter
+      post.update(comments_counter: post.comments.count)
+    end
   end
 end
