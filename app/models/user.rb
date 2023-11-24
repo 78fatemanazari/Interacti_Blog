ac9e5294335fb864982b20714287893e9a60803d
fatemanazari <fatemanazari78@gmail.com>
2023-11-24 11:08:03 +0330

    Update user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,14 @@
+# app/models/user.rb
 class User < ApplicationRecord
   has_many :posts, foreign_key: 'author_id', dependent: :destroy
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
 
   validates :name, presence: true
-  validates :photo_url, presence: true, format: { with: %r{\Ahttps?://.*\z}, message: 'must be a valid URL' }
+  validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
+  # Updated attribute name to photo_url
+  validates :photo_url, presence: true, format: { with: /\Ahttps?:\/\/.*\z/, message: "must be a valid URL" }
   validates :bio, length: { maximum: 500 }
 
   def three_most_recent_posts
