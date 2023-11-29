785587d80eeb7b67c1393c7868d306530636914d
fatemanazari <fatemanazari78@gmail.com>
2023-11-29 21:40:23 +0330

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,7 @@ Rails.application.routes.draw do
   resources :users, only: [:index, :show] do
     resources :posts, only: [:index, :show]
   end
+
+  get "up" => "rails/health#show", as: :rails_health_check
 end
 
