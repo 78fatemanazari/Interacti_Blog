24214531b86c929dce9e204dd8cddb2e1165a4fb
fatemanazari <fatemanazari78@gmail.com>
2023-12-06 22:21:36 +0330

    Remove current user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,10 @@
 class ApplicationController < ActionController::Base
-  before_action :set_current_user
+  before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
-  def set_current_user
-    @current_user = current_user
-  end
+  protected
 
-  helper_method :current_user
-
-  def current_user
-    User.first # Replace this with your logic to retrieve the current user
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[email name password password_confirmation])
   end
 end
